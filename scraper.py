fe3563fe60b5cad95cb29f83e2e78428fa4f2df0
miguelmacis-afk <miguelmacis@gmail.com>
2026-09-13 07:35:52 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ def es_url_valida(url):
     dominios_ignorados = [
         "facebook.com", "messenger.com", "fb.com", 
         "fb.me", "fbcdn.net", "instagram.com", "whatsapp.com",
-        "meta."
+        "meta.com", "meta.ai", "meta.org"
     ]
     url_lower = url.lower()
     return not any(dominio in url_lower for dominio in dominios_ignorados)
